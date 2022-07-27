4bb943f25449b9210fafedcf408754464e7e52d3
Muhammad Qazafi Sheikh <qazafi.sheikh121@gmail.com>
2022-07-27 21:14:34 +0500

    Delete Links.js

--- Component/Links/Links.js
+++
@@ -1,7 +0,0 @@
-const link = 'https://2ed0be2037d9.ngrok.io/'
-
-const sendVerficationCode = 'api/v1/student/sendVerficationCode'
-const register = 'api/v1/student/register'
-const signin = 'api/v1/student/signin'
-
-export { link, sendVerficationCode, register, signin }
